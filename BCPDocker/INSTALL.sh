9c32c604bc0872c22ac00852923939ec3e42b794
legraina <antoine.legrain@gmail.com>
2023-10-04 23:56:35 -0400

    Add publish docker actions and automatically download last config files.

--- BCPDocker/INSTALL.sh
+++ BCPDocker/INSTALL.sh
@@ -9,7 +9,9 @@ fi
 
 # Install BCP
 cd $DIR
-svn co https://projects.coin-or.org/svn/Bcp/stable/1.4 Bcp-1.4
+wget https://github.com/coin-or/Bcp/archive/refs/tags/releases/1.4.4.tar.gz
+tar -xf 1.4.4.tar.gz
+mv Bcp-releases-1.4.4 Bcp-1.4
 mkdir -p Bcp-1.4/build
 BCP_ARGS=""
 BUILD_TYPE="Release"
